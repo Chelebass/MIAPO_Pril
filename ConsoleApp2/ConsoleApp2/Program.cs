3aebe0ee0d4e8e23bc1f060c6fc72dff5c6c3c8b
Chelebass <Aninanisenp@gmail.com>
2024-04-03 16:42:17 +0700

    Added Func1

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,5 +7,11 @@ namespace ConsoleApp2
         {
             
         }
+        public static double returnDel(double a, double b)
+        {
+            double aANDb = a / b;
+            return aANDb;
+        }
+        
     }
 }
