b6238614519d6605bac518171d0a69324b8b2ddb
Chelebass <Aninanisenp@gmail.com>
2024-04-03 16:53:34 +0700

    Added Func4

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,7 +5,39 @@ namespace ConsoleApp2
     {
         static void Main()
         {
+            Console.WriteLine("Введите число 1");
+            double num1 = Convert.ToInt32(Console.ReadLine());
             
+            Console.WriteLine("Введите число 2");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            double numRes = 0;
+            
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1) + \n2) * \n 3) - \n 4) /");
+            
+            string choosedItem = Console.ReadLine();
+
+            switch (choosedItem)
+            {
+                case "+":
+                    numRes = returnPlus(num1, num2);
+                    break;
+                
+                case "*":
+                    numRes = returnUmn(num1, num2);
+                    break;
+                    
+                case "-":
+                    numRes = returnMin(num1, num2);
+                    break;
+                
+                case "/":
+                    numRes = returnDel(num1, num2);
+                    break;
+            }
+            
+            Console.WriteLine(numRes);
         }
         
         public static double returnUmn(double a, double b)
